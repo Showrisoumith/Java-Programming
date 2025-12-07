1d0ed7211ebfc9f48e4ae79e18b692b6190328f5
Soumith <147472509+Showrisoumith@users.noreply.github.com>
2025-12-07 11:15:09 +0530

    Time: 0 ms (100%), Space: 44.9 MB (37.39%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
@@ -1,9 +1,15 @@
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
-        int s1 = Arrays.stream(nums1).sum();
-        int s2 = Arrays.stream(nums2).sum();
-        int diff = s2-s1;
-        return (diff/nums1.length);
+        // int s1 = Arrays.stream(nums1).sum();
+        // int s2 = Arrays.stream(nums2).sum();
+        // int diff = s2-s1;
+        // return (diff/nums1.length);
+        int s1 =0,s2=0;
+        for(int i  = 0;i<nums1.length;i++){
+            s1+=nums1[i];
+            s2+=nums2[i];
+        }
+        return (s2-s1)/nums1.length;
 
     }
 }
